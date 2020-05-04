e37cbdfeb4475e42ef891d16f49cd20c51da0e44
amihut <amihut@EN610723>
2020-05-04 11:00:37 +0300

    organize routes

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const EditExpensePage = () => <div>This is from my Edit Expense Component</div>;
+
+export default EditExpensePage;
---
+++ src/components/HelpPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HelpPage = () => <div>This is from my Help Component</div>;
+
+export default HelpPage;
