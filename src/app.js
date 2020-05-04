f3038d9ec12487232f1c73fd19f142c5538e2b37
amihut <amihut@EN610723>
2020-05-04 10:05:53 +0300

    set up react route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>test</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>This is from my Dashboard Component</div>
+);
+const AddExpensePage = () => <div>This is from my Add Expense Component</div>;
+const EditExpensePage = () => <div>This is from my Edit Expense Component</div>;
+const HelpPage = () => <div>This is from my Help Component</div>;
+const NotFoundPage = () => <div>404!</div>;
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={ExpenseDashboardPage} exact={true} />
+      <Route path='/create' component={AddExpensePage} />
+      <Route path='/edit' component={EditExpensePage} />
+      <Route path='/help' component={HelpPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
