e4ba96103fd664dc67abb9ac75b3bfb9b52f62d1
amihut <amihut@EN610723>
2020-05-06 12:50:27 +0300

    add controlled inputs for filters and remove buttons for expenses

--- src/app.js
+++ src/app.js
@@ -23,8 +23,6 @@ setTimeout(() => {
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-// console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
