03ae4db40df604be64620648153e301a884f0ed7
amihut <amihut@EN610723>
2020-05-06 12:17:47 +0300

    render individual expenses

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import ExpenseList from './ExpenseList';
+
 const ExpenseDashboardPage = () => (
-  <div>This is from my Dashboard Component</div>
+  <div>
+    <ExpenseList />
+  </div>
 );
 
 export default ExpenseDashboardPage;
